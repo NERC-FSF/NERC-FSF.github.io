4ee87c1baa4d5aac0611f41670c49f0354895464
NERC FSF <60514125+NERC-FSF@users.noreply.github.com>
2024-02-27 14:47:30 +0000

    Add files via upload

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,13 +1,7 @@
 var store = [{
-        "title": "NERC -- Announcment of Recommissioing",
-        "excerpt":"Tuesday 27th February 2024 We are pleased to announce that the facility has been recommissioned until 2031.  ","categories": [],
+        "title": "NERC FSF -Recommissioning of Service until March 2031",
+        "excerpt":"Tuesday 27th February 2024 The National Environment Research Council has today confirmed that a £70 million investment has been secured for the continued operation until March 2031 of seven NERC facilities, which includes the Field Spectroscopy Facility. This means that FSF will be able to continue in our provision of...","categories": [],
         "tags": [],
         "url": "/announcment-of-recommissioning/",
         "teaser": null
-      },{
-        "title": "Test Tutorial",
-        "excerpt":"A test page for tutorials.      Step by step   Guides   On processing   With…       ","categories": [],
-        "tags": [],
-        "url": "/tutorials/Test-Tutorial/",
-        "teaser": null
-      }]
+      },]
