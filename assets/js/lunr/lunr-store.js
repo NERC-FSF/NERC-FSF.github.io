b10841633ec2f90e4b7aebe7406d6923f6ef1129
NERC FSF <60514125+NERC-FSF@users.noreply.github.com>
2024-02-27 12:13:21 +0000

    Add files via upload

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "NERC -- Announcment of Recommissioing",
+        "excerpt":"Tuesday 27th February 2024 We are pleased to announce that the facility has been recommissioned until 2031.  ","categories": [],
+        "tags": [],
+        "url": "/announcment-of-recommissioning/",
+        "teaser": null
+      },{
+        "title": "Test Tutorial",
+        "excerpt":"A test page for tutorials.      Step by step   Guides   On processing   With…       ","categories": [],
+        "tags": [],
+        "url": "/tutorials/Test-Tutorial/",
+        "teaser": null
+      }]
